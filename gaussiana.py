ee497889226c67655c5af047e51d4c029df49e1c
Iris Ochoa <gaby8a98@hotmail.com>
2020-12-06 11:47:52 -0600

    Agregué función gaussiana con nuevos valores (amarilla)

--- gaussiana.py
+++ gaussiana.py
@@ -24,12 +24,16 @@ s3=3
 
 s4=2
 m4=1
+s5=6
+m5=3
 
 plt.plot(x_valores,Gaussiana(x_valores,s1,m1), color='turquoise', label='μ=3, σ=1')
 plt.plot(x_valores,Gaussiana(x_valores,s2,m2), color='deeppink', label='μ=0, σ=0.5')
 plt.plot(x_valores,Gaussiana(x_valores,s3,m3), color='purple', label='μ=2, σ=3')
 #Cambios git
 plt.plot(x_valores,Gaussiana(x_valores,s4,m4), color='green')
+plt.plot(x_valores,Gaussiana(x_valores,s5,m5), color='yellow')
+
 
 
 plt.legend(loc=1)
